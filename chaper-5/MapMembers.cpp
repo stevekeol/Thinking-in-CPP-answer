40a15108edf46b5eda3e340e9083de2b85b7f623
stevekeol <stevekeol.x@gmail.com>
2017-06-26 14:10:01 +0800

    5-8

---
+++ chaper-5/MapMembers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+class Mapped{
+	int x;
+
+protected:
+	int y;
+
+public:
+	int z;
+
+	void showMap() {
+		cout << "x is at " << &x << endl;
+		cout << "y is at " << &y << endl;
+		cout << "z is at " << &z << endl;
+	}
+};
+
+int main() {
+	Mapped m;
+	m.showMap();
+
+	system("pause");
+}
